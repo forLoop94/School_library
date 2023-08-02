033445dfa950922f56581e634e75c0b24a7133be
forLoop94 <charlesuwem94@gmail.com>
2023-08-02 11:10:32 +0100

    Fix bug in Nameable's correct_name method

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
   def correct_name
-    raise NoImplementedError, "#{self.class} has not implemented the 'correct_name' method."
+    raise NotImplementedError, "#{self.class} has not implemented the 'correct_name' method."
   end
 end
