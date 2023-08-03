1f9a5c5f0db79e2eb1a119ad0f86519641383cfe
forLoop94 <charlesuwem94@gmail.com>
2023-08-03 23:47:49 +0100

    Add method to list rentals by ID of person

--- app.rb
+++ app.rb
@@ -21,7 +21,21 @@ class App
       @people.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
     end
   end
+
+  def list_rentals_by_person_id
+    puts "ID of person: "
+    id = gets.chomp.to_i
+    target_person = @people.find { |person| person.id == id }
+    if target_person.nil
+      puts "No person available"
+    else
+      puts "Rentals"
+      puts "The given person doesn't rent any book" if person.rentals.empty?
+      person.rentals.each { |rental| puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}" }
+    end
+  end
 end
 
 app = App.new()
 app.list_books
+app.list_rentals_by_person_id
