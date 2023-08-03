1dc536a051b38b395f705a8180f20968d60c7b5b
forLoop94 <charlesuwem94@gmail.com>
2023-08-03 10:16:45 +0100

    Create the belong-to side of Rental and Person

--- rental.rb
+++ rental.rb
@@ -1,13 +1,19 @@
 class Rental
-  attr_accessor :date, :book
+  attr_accessor :date, :book, :person
 
-  def initialize(date, book)
+  def initialize(date, book, person)
     @date = date
     @book = book
+    @person = person
   end
 
   def add_book(book)
     @book = book
     book.rentals << self unless book.rentals.includes?(self)
+  end
+
+  def add_person(person)
+    @person = person
+    person.rentals << self unless person.rentals.include?(self)
   end
 end
